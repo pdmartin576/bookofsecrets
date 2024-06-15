d217d4c1825dff3e3ad21a5214284859a4907691
pdmartin576 <pdmartin576@gmail.com>
2024-06-15 09:53:18 -0700

    Final Score Display

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const scores = [0, 0, 0, 0, 0, 0]; // [groupA, groupB, groupC, groupD, groupE, groupF]
+const scores = [0, 0, 0, 0, 0, 0]; // [Storm, Ice, Fire, Death, Myth, Life]
 
 function submitQuiz() {
     const questions = document.querySelectorAll('.question');
@@ -20,28 +20,35 @@ function submitQuiz() {
 
     switch (groupIndex) {
         case 0:
-            groupName = 'groupA';
+            groupName = 'Storm';
             break;
         case 1:
-            groupName = 'groupB';
+            groupName = 'Ice';
             break;
         case 2:
-            groupName = 'groupC';
+            groupName = 'Fire';
             break;
         case 3:
-            groupName = 'groupD';
+            groupName = 'Death';
             break;
         case 4:
-            groupName = 'groupE';
+            groupName = 'Myth';
             break;
         case 5:
-            groupName = 'groupF';
+            groupName = 'Life';
             break;
         default:
-            groupName = 'No group selected';
+            groupName = '...uhhh you didn\'t select anything, did you?';
     }
 
     document.getElementById('quiz').style.display = 'none';
     document.getElementById('result').style.display = 'block';
-    document.getElementById('resultText').innerText = `You belong to ${groupName}`;
+    document.getElementById('resultText').innerText = `You belong to the School of ${groupName}.`;
+    // Update the scores in the table
+    document.getElementById('stormScore').innerText = scores[0];
+    document.getElementById('iceScore').innerText = scores[1];
+    document.getElementById('fireScore').innerText = scores[2];
+    document.getElementById('deathScore').innerText = scores[3];
+    document.getElementById('mythScore').innerText = scores[4];
+    document.getElementById('lifeScore').innerText = scores[5];
 }
